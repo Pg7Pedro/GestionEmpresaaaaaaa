d9260d8737bc6b82c3f2b157bfe3cde00efed37a
Pg7Pedro <Pg91999@hotmail.com>
2021-09-30 10:23:09 +0200

    Initial commit

---
+++ src/gestionempresa/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionempresa;
+
+public class Test {
+    public static void main(String[] args) {
+        Empleados E1 =new Empleados (3500, "Luis", "Puerta del angel 31", 'm', 32);
+        System.out.println(E1);
+        Empleados E2 =new Empleados ();
+        System.out.println(E2);
+        Cliente C1 = new Cliente();
+        System.out.println(C1);
+        Cliente C2 = new Cliente();
+        System.out.println(C2);
+        
+    }
+}
